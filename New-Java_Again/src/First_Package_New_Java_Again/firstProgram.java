9e688f67c58eee610bbc383335829ea58e244dfc
Govind9066 <gadekargovind575@gmail.com>
2025-06-23 21:16:41 +0530

    Commit can done from the intelliJ IDEA

--- New-Java_Again/src/First_Package_New_Java_Again/firstProgram.java
+++ New-Java_Again/src/First_Package_New_Java_Again/firstProgram.java
@@ -4,5 +4,6 @@ public class firstProgram {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("This is my First HELLO WORLD program form the New Java Again");
+        System.out.println("Checking the github commit which can i do on the Intellige Idea");
     }
 }
