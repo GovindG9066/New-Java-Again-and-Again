3cf9381a75214bfa454348cdf052aa2fd1ec4a3c
Govind9066 <gadekargovind575@gmail.com>
2025-06-24 23:59:48 +0530

    function pe projects starts

---
+++ New-Java_Again/src/Functions_Methods/StudentMarksheetGeneratorProject1.java
@@ -0,0 +1,17 @@
+package Functions_Methods;
+
+import java.util.Scanner;
+
+
+public class StudentMarksheetGeneratorProject1 {
+    Scanner sc=new Scanner(System.in);
+
+    public static void input(){
+        System.out.println();
+    }
+    public static void main(String[] args) {
+
+
+
+    }
+}
