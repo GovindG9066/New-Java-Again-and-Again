cd3986d907042566c5200ab5ac0368ce3e024552
Govind9066 <gadekargovind575@gmail.com>
2025-06-28 22:51:09 +0530

    Palindrome Number program

---
+++ New-Java_Again/src/First_Package_New_Java_Again/palandromNo.java
@@ -0,0 +1,20 @@
+package First_Package_New_Java_Again;
+
+public class palandromNo {
+    public static void main(String[] args) {
+        int num=121;
+        int original=num;
+        int reverse=0;
+
+        while(num!=0){
+            int digit=num%10;
+            reverse=reverse*10+digit;
+            num/=10;
+        }
+        if (original==reverse){
+            System.out.println(original+" is Palindrome Number");
+        }else{
+            System.out.println(original+" is not Palindrome Number");
+        }
+    }
+}
