158ff99566251459727ef91686a5d05b215996ad
Govind9066 <gadekargovind575@gmail.com>
2025-06-30 20:51:01 +0530

    largestNo in array and Second LargestNo in array

--- New-Java_Again/src/DADA/SecondLargestNoInArray.java
+++ New-Java_Again/src/DADA/SecondLargestNoInArray.java
@@ -1,7 +1,27 @@
 package DADA;
 
 public class SecondLargestNoInArray {
-    public static void main(String[] args) {
+    public void SecondLargestNo(){
+        int arr[]={10,85,35,64,12};
+        int sortedarr[]=new int[5];
+        int largest=arr[0];
+        for (int i=0;i< arr.length-1;i++){
+            if (arr[i]>largest){
+                largest=arr[i];
+            }
+        }
+        int second=Integer.MIN_VALUE;
+        for (int i=0;i< arr.length-1;i++){
+            if (arr[i]!=largest && arr[i]>second){
+                second=arr[i];
 
+            }
+        }
+        System.out.println(largest);
+        System.out.println("Second :"+second);
+    }
+    public static void main(String[] args) {
+        SecondLargestNoInArray LNO=new SecondLargestNoInArray();
+        LNO.SecondLargestNo();
     }
 }
