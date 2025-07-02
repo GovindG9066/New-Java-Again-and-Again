d16a6c20fcff99e4b56b2aaf91070c607eea2ce5
Govind9066 <gadekargovind575@gmail.com>
2025-07-02 19:12:55 +0530

    factorial of the number

--- New-Java_Again/src/com/LectorsBySmartProgramming/MultiplicationTable.java
+++ New-Java_Again/src/com/LectorsBySmartProgramming/MultiplicationTable.java
@@ -5,11 +5,22 @@ import java.util.Scanner;
 public class MultiplicationTable {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the Number the No : ");
-        int num=sc.nextInt();
+        boolean flag=true;
+        while (flag){
+            System.out.println("Enter the Number the No : ");
+            int num=sc.nextInt();
 
-        for (int i=1;i<=10;i++){
-            System.out.println(num+" * "+i+" = "+num*i);
+            for (int i=1;i<=10;i++){
+                System.out.println(num+" * "+i+" = "+num*i);
+            }
+            System.out.println("Enter Your Choice :(Y/N)");
+            char yn=sc.next().toUpperCase().charAt(0);
+            if (yn=='N'){
+                System.out.println("Exit the program");
+                break;
+            }else {
+                System.out.println("Enter Again");
+            }
         }
     }
 }
