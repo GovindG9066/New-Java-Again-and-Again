fc4e9015958a6179d783ed98d02060f568e98c72
Govind9066 <gadekargovind575@gmail.com>
2025-07-02 23:16:43 +0530

    reverse of String

---
+++ New-Java_Again/src/com/LectorsBySmartProgramming/ReverseString.java
@@ -0,0 +1,12 @@
+package com.LectorsBySmartProgramming;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String name="Govind";
+
+        for (int i=name.length()-1;i>=0;i--){
+            System.out.print(name.charAt(i));
+
+        }
+    }
+}
