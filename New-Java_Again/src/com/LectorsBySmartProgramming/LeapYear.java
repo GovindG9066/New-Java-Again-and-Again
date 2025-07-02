b590cbf2b9c24167e3d0e76b0b01b8d1aefdfc82
Govind9066 <gadekargovind575@gmail.com>
2025-07-02 17:22:04 +0530

    by the leactor learn the new program of check the year is a leap year or not

--- New-Java_Again/src/com/LectorsBySmartProgramming/LeapYear.java
+++ New-Java_Again/src/com/LectorsBySmartProgramming/LeapYear.java
@@ -2,6 +2,12 @@ package com.LectorsBySmartProgramming;
 
 public class LeapYear {
     public static void main(String[] args) {
+        int year=2015;
 
+        if (year%400==0 || year%4==0 && year%100!=0){
+            System.out.println(year+" is the Leap year");
+        }else {
+            System.out.println(year+" is not an leap year");
+        }
     }
 }
