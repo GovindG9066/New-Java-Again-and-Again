bc219182335daa64ae468867e6d313da370b16d4
Govind9066 <gadekargovind575@gmail.com>
2025-07-04 22:55:41 +0530

    practice program reverse program

---
+++ New-Java_Again/src/Practice/reverseNo.java
@@ -0,0 +1,15 @@
+package Practice;
+
+public class reverseNo {
+    public static void main(String[] args) {
+        int num = 123;
+        int rev=0;
+
+        while (num!=0){
+            int digit=num%10;
+            rev=rev*10+digit;
+            num/=10;
+        }
+        System.out.println(rev);
+    }
+}
