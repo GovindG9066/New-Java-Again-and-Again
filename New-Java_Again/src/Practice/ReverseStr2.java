295740ed2b1ffbe7c754201a242049cdb691cd23
Govind9066 <gadekargovind575@gmail.com>
2025-07-08 12:12:17 +0530

    check the prime No

---
+++ New-Java_Again/src/Practice/ReverseStr2.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class ReverseStr2 {
+    public static void main(String[] args) {
+        String str="dnivoG";
+
+
+        for (int i=str.length()-1;i>=0;i--){
+            System.out.print(str.charAt(i));
+        }
+
+    }
+}
---
+++ New-Java_Again/src/com/LectorsBySmartProgramming/PrimeNo.java
@@ -0,0 +1,19 @@
+package com.LectorsBySmartProgramming;
+
+public class PrimeNo {
+    public static void main(String[] args) {
+        int NO=7;
+        int temp=0;
+
+        for (int i=2;i<=NO-1;i++){
+            if (NO%i==0){
+                temp+=1;
+            }
+        }
+        if (temp>0){
+            System.out.println(NO+" is Not an Prime No");
+        }else {
+            System.out.println(NO+" is Prime No");
+        }
+    }
+}
