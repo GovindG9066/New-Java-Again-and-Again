7253542a8f02fca33fb6c17965cf723f58649522
Govind9066 <gadekargovind575@gmail.com>
2025-07-11 21:34:20 +0530

    find the factor of the number program

---
+++ New-Java_Again/src/Practice1/FactroOfNum.java
@@ -0,0 +1,13 @@
+package Practice1;
+
+public class FactroOfNum {
+    public static void main(String[] args) {
+        int num=10;
+
+        for (int i=1;i<=num;i++){
+            if (num%i==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
