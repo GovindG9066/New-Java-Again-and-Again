cc9e851e7d620c8d1fb1302de4a720324a5bbd1c
Govind9066 <gadekargovind575@gmail.com>
2025-07-11 20:44:31 +0530

    create new package and add the use all the programs in the package

---
+++ New-Java_Again/src/Practice1/fib1.java
@@ -0,0 +1,18 @@
+package Practice1;
+
+public class fib1 {
+    public static void main(String[] args) {
+        int first=0,second=1,count=10;
+        System.out.println("Fib series :");
+        System.out.println(first);
+        System.out.println(second);
+
+        for (int i=2;i<=count-1;i++){
+            int next=first+second;
+            first=second;
+            second=next;
+            System.out.println(next);
+        }
+
+    }
+}
