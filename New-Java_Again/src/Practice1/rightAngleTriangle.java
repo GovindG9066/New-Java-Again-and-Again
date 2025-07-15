d558127b76d016af8248a79dbb7bf2e25415952b
Govind9066 <gadekargovind575@gmail.com>
2025-07-15 20:15:00 +0530

    practice again in practice2 package adding the program Right angle triangle

---
+++ New-Java_Again/src/Practice1/rightAngleTriangle.java
@@ -0,0 +1,14 @@
+package Practice1;
+
+public class rightAngleTriangle {
+    public static void main(String[] args) {
+        int count=5;
+
+        for (int i=1;i<=count;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
