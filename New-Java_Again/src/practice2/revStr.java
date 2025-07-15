5c4430478b12e1a406c01d5bc43ff2e2d1ca6551
Govind9066 <gadekargovind575@gmail.com>
2025-07-15 19:38:59 +0530

    practice again in practice2 package adding the program reverse String

---
+++ New-Java_Again/src/practice2/revStr.java
@@ -0,0 +1,11 @@
+package practice2;
+
+public class revStr {
+    public static void main(String[] args) {
+        String name="Govind";
+
+        for (int i=name.length()-1;i>=0;i--){
+            System.out.print(name.charAt(i));
+        }
+    }
+}
