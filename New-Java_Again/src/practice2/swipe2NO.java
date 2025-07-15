10106a896f76b5322ebef45b243fd577e5dd1416
Govind9066 <gadekargovind575@gmail.com>
2025-07-15 19:47:01 +0530

    practice again in practice2 package adding the program swipe 2 Numbers without using 3rd variable

---
+++ New-Java_Again/src/practice2/swipe2NO.java
@@ -0,0 +1,16 @@
+package practice2;
+
+public class swipe2NO {
+    public static void main(String[] args) {
+        int first=123;
+        int second=356;
+
+        first=first+second;
+        second=first-second;
+        first=first-second;
+
+        System.out.println(first);
+        System.out.println(second);
+
+    }
+}
