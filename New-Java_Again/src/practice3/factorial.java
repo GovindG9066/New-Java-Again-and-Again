4cd23b26fabb24b52ff9711656ffb2701905fb3b
Govind9066 <gadekargovind575@gmail.com>
2025-07-16 20:39:46 +0530

    creating new package practice3 and add practice the program inside the package like factorial of the number

---
+++ New-Java_Again/src/practice3/factorial.java
@@ -0,0 +1,12 @@
+package practice3;
+
+public class factorial {
+    public static void main(String[] args) {
+        int count=5;
+        int fact=1;
+        for (int i=1;i<=count;i++){
+            fact=fact*i;
+        }
+        System.out.println(fact);
+    }
+}
