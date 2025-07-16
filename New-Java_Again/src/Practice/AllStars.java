c82c2f67846516e9be6066127076a3ccfa4f3ea4
Govind9066 <gadekargovind575@gmail.com>
2025-07-16 16:27:30 +0530

    trying to add all programs in one program

---
+++ New-Java_Again/src/Practice/AllStars.java
@@ -0,0 +1,45 @@
+package Practice;
+
+public class AllStars {
+    public static void main(String[] args) {
+        int count=4;
+        for (int i=1;i<=count;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------------------------------------------------------");
+        for (int i=1;i<=count;i++){
+            for (int j=count;j>=i;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------------------------------------------------------");
+
+        for (int i=1;i<=count;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i=2;i<=count;i++){
+            for (int j=count;j>=i;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------------------------------------------------------");
+
+        for (int i=1;i<=count;i++){
+            for (int j=count-1;j>=i;j--){
+                System.out.print("  ");
+            }
+            for (int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
