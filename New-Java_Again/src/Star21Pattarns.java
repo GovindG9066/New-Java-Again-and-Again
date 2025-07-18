9cd15202100bba6f80124fbbcc1051df99474380
Govind9066 <gadekargovind575@gmail.com>
2025-07-18 23:05:48 +0530

    Simple Right Angle Triangle Printed

--- New-Java_Again/src/Star21Pattarns.java
+++ New-Java_Again/src/Star21Pattarns.java
@@ -1,2 +1,17 @@
 public class Star21Pattarns {
+    public static void main(String[] args) {
+        int  count =5;
+        System.out.println("1.");
+
+       for (int i=1;i<=count;i++){
+           for (int j=1;j<=i;j++){
+               System.out.print("* ");
+           }
+           System.out.println();
+       }
+
+        System.out.println("2.");
+
+
+    }
 }
