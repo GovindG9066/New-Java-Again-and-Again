97d2f0df294b0e7ddcad530be096f8bf0c49bc85
Govind9066 <gadekargovind575@gmail.com>
2025-07-18 23:51:56 +0530

    combination of up side down combination of up side down triangle triangle with middle column and combination of 2 triangle with the middle triangle pattarn look like :    * * * * * * * * *       * * * * * * *         * * * * *           * * *             *           * * *         * * * * *       * * * * * * *     * * * * * * * * *

--- New-Java_Again/src/Star21Pattarns.java
+++ New-Java_Again/src/Star21Pattarns.java
@@ -148,5 +148,33 @@ public class Star21Pattarns {
             System.out.println();
         }
 
+        System.out.println("----------------------------------------------------------------");
+
+        System.out.println("11.");
+
+        for (int i=1;i<=count;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("  ");
+            }
+            for (int k=count;k>=i;k--){
+                System.out.print("* ");
+            }
+            for (int l=count-1;l>=i;l--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i=2;i<=count;i++){
+            for (int j=count;j>=i;j--){
+                System.out.print("  ");
+            }
+            for (int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            for (int l=2;l<=i;l++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
