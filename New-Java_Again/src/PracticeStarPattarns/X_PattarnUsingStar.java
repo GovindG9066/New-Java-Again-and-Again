6e3a284ed7b339680d803e881dd33bc5a9066192
Govind9066 <gadekargovind575@gmail.com>
2025-07-18 16:52:01 +0530

    print an X pattarns

--- New-Java_Again/src/PracticeStarPattarns/X_PattarnUsingStar.java
+++ New-Java_Again/src/PracticeStarPattarns/X_PattarnUsingStar.java
@@ -5,15 +5,41 @@ public class X_PattarnUsingStar {
         int count=4;
 
         for (int i=1;i<=count;i++){
-            for (int j=2;j<=i;j++){
+            for (int j=1;j<=i;j++){
                 System.out.print("  ");
             }
             for (int k=count;k>=i;k--){
-                if (i>=1 && k>1){
+                if (k<count){
                     System.out.print("  ");
                 }else {
-                System.out.print("* ");
-
+                    System.out.print("* ");
+                }
+            }
+            for (int l=count-1;l>=i;l--){
+                if (l!=i){
+                    System.out.print("  ");
+                }else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i=2;i<=count;i++){
+            for (int j=count;j>=i;j--){
+                System.out.print("  ");
+            }
+            for (int k=1;k<=i;k++){
+                if (k>=2){
+                    System.out.print("  ");
+                }else {
+                    System.out.print("* ");
+                }
+            }
+            for (int l=2;l<=i;l++){
+                if (l<=i-1){
+                    System.out.print("  ");
+                }else {
+                    System.out.print("* ");
                 }
             }
             System.out.println();
