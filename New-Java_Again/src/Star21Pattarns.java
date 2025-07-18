b6ebff5a29b5b573784a12686fc64a1a017f68ff
Govind9066 <gadekargovind575@gmail.com>
2025-07-19 00:56:40 +0530

    right angle triangle ,first triangle empty, diagonal pattarn pattarn look like :
    *
      *
        *
          *
            *

--- New-Java_Again/src/Star21Pattarns.java
+++ New-Java_Again/src/Star21Pattarns.java
@@ -328,5 +328,22 @@ public class Star21Pattarns {
             System.out.println();
         }
 
+        System.out.println("----------------------------------------------------------------");
+
+        System.out.println("18.");
+
+        for (int i=1;i<=count;i++){
+            for (int j=2;j<=i;j++){
+                System.out.print("  ");
+            }
+            for (int k=count;k>=i;k--){
+                if (k>=count){
+                    System.out.print("* ");
+                }else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
