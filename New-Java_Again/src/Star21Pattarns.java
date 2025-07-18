0d43012988cb191e645bd87b566b8ff2b727c1e4
Govind9066 <gadekargovind575@gmail.com>
2025-07-19 00:46:38 +0530

    combination of the both triangle having the middle diagonal make the diamond shape which is empty inside pattarn look like :
            *
          *   *
        *       *
      *           *
    *               *
      *           *
        *       *
          *   *
            *

--- New-Java_Again/src/Star21Pattarns.java
+++ New-Java_Again/src/Star21Pattarns.java
@@ -254,5 +254,50 @@ public class Star21Pattarns {
             }
             System.out.println();
         }
+
+        System.out.println("----------------------------------------------------------------");
+
+        System.out.println("15.");
+
+        for (int i=1;i<=count;i++){
+            for (int j=count-1;j>=i;j--){
+                System.out.print("  ");
+            }
+            for (int k=1;k<=i;k++){
+                if (k>=2){
+                    System.out.print("  ");
+                }else {
+                    System.out.print("* ");
+                }
+            }
+            for (int l=2;l<=i;l++){
+                if (l==i){
+                    System.out.print("*");
+                }else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i=2;i<=count;i++){
+            for (int j=2;j<=i;j++){
+                System.out.print("  ");
+            }
+            for (int k=count;k>=i;k--){
+                if (k==count){
+                    System.out.print("* ");
+                }else {
+                    System.out.print("  ");
+                }
+            }
+            for (int l=count-1;l>=i;l--){
+                if (l==i){
+                    System.out.print("* ");
+                }else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
