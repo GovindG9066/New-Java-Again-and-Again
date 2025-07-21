fd43205177e445cbac188bdcab44504685020735
Govind9066 <gadekargovind575@gmail.com>
2025-07-21 23:08:16 +0530

    Pyramid triangle Up Side Down with center diagonal star pattarn

--- New-Java_Again/src/StarPattarnsPractice1/AllTriangle.java
+++ New-Java_Again/src/StarPattarnsPractice1/AllTriangle.java
@@ -77,5 +77,19 @@ public class AllTriangle {
             System.out.println();
         }
         System.out.println();
+
+        for (int i=1;i<=count;i++){
+            for (int j=2;j<=i;j++){
+                System.out.print("  ");
+            }
+            for (int k=count;k>=i;k--){
+                System.out.print("* ");
+            }
+            for (int l=count-1;l>=i;l--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
