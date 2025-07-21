e8028ce3e1a1e05974f1713d52dc1c7001f5dae6
Govind9066 <gadekargovind575@gmail.com>
2025-07-21 22:58:11 +0530

    Up Side Down pytamid triangle

--- New-Java_Again/src/StarPattarnsPractice1/AllTriangle.java
+++ New-Java_Again/src/StarPattarnsPractice1/AllTriangle.java
@@ -44,10 +44,20 @@ public class AllTriangle {
         System.out.println();
 
         for (int i=1;i<=count;i++){
-            for (int j=count;j>=i;j--){
+            for (int j=count-1;j>=i;j--){
                 System.out.print(" ");
             }
             for (int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i=1;i<=count;i++){
+            for (int j=2;j<=i;j++){
+                System.out.print(" ");
+            }
+            for (int k=count;k>=i;k--){
                 System.out.print("* ");
             }
             System.out.println();
