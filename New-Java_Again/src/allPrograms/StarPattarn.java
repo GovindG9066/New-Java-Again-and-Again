ce389e0a737624fd810ad1e65cd5e5781973255b
Govind9066 <gadekargovind575@gmail.com>
2025-07-23 00:07:09 +0530

    new package allPrograms pyramid pattarn Up Side Down

--- New-Java_Again/src/allPrograms/StarPattarn.java
+++ New-Java_Again/src/allPrograms/StarPattarn.java
@@ -53,5 +53,15 @@ public class StarPattarn {
         }
         System.out.println();
 
+        for (int i=1;i<=count;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for (int k=count;k>=i;k--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
