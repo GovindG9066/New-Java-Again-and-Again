d1116801612f8ea58f55d454854869fa2e0bbf72
Govind9066 <gadekargovind575@gmail.com>
2025-07-22 19:38:19 +0530

    new package allPrograms armstrong no

---
+++ New-Java_Again/src/allPrograms/armstrongNo.java
@@ -0,0 +1,20 @@
+package allPrograms;
+
+public class armstrongNo {
+    public static void main(String[] args) {
+        int num=153;
+        int original=num;
+        double arm=0;
+
+        while (num != 0){
+            int digit=num%10;
+            arm=arm+Math.pow(digit,3);
+            num/=10;
+        }
+        if (original==arm){
+            System.out.println("Yes");
+        }else {
+            System.out.println("NO");
+        }
+    }
+}
