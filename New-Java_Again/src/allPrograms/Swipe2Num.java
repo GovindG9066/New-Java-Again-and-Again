999fd4c441148a6439bd64ae9b28962fca5f9028
Govind9066 <gadekargovind575@gmail.com>
2025-07-22 23:10:52 +0530

    new package allPrograms swipe 2 numbers without using 3rd variable

---
+++ New-Java_Again/src/allPrograms/Swipe2Num.java
@@ -0,0 +1,14 @@
+package allPrograms;
+
+public class Swipe2Num {
+    public static void main(String[] args) {
+        int first=5,second=37;
+
+        first=first+second;
+        second=first-second;
+        first=first-second;
+
+        System.out.println(first);
+        System.out.println(second);
+    }
+}
