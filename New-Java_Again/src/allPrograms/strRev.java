ebdf6574376ae9efd0e70ea1f9ae19a669275ccf
Govind9066 <gadekargovind575@gmail.com>
2025-07-22 21:10:52 +0530

    new package allPrograms String Reverse

---
+++ New-Java_Again/src/allPrograms/strRev.java
@@ -0,0 +1,12 @@
+package allPrograms;
+
+public class strRev {
+    public static void main(String[] args) {
+        String name="Govind";
+
+        for (int i=name.length()-1;i>=0;i--){
+            System.out.print(name.charAt(i));
+        }
+
+    }
+}
