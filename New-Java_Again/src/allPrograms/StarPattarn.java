8cf691bb8ff7e435c4f5a01819e74fb04a81b3df
Govind9066 <gadekargovind575@gmail.com>
2025-07-23 00:11:00 +0530

    new package allPrograms pyramid with center diagonal

--- New-Java_Again/src/allPrograms/StarPattarn.java
+++ New-Java_Again/src/allPrograms/StarPattarn.java
@@ -54,10 +54,24 @@ public class StarPattarn {
         System.out.println();
 
         for (int i=1;i<=count;i++){
-            for (int j=1;j<=i;j++){
+            for (int j=2;j<=i;j++){
                 System.out.print(" ");
             }
             for (int k=count;k>=i;k--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i=1;i<=count;i++){
+            for (int j=count-1;j>=i;j--){
+                System.out.print("  ");
+            }
+            for (int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            for (int l=2;l<=i;l++){
                 System.out.print("* ");
             }
             System.out.println();
