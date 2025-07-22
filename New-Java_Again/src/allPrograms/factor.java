551f878b243dd19f8e526bf8ddd47f7e2b685700
Govind9066 <gadekargovind575@gmail.com>
2025-07-22 23:13:38 +0530

    new package allPrograms factor of the number

---
+++ New-Java_Again/src/allPrograms/factor.java
@@ -0,0 +1,13 @@
+package allPrograms;
+
+public class factor {
+    public static void main(String[] args) {
+        int num=15;
+
+        for (int i=1;i<=num;i++){
+            if (num%i==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
