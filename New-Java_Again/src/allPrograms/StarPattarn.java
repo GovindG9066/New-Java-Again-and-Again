00cfebd1cd3dee83fd691c4047a01cabd6d9a7b5
Govind9066 <gadekargovind575@gmail.com>
2025-07-23 00:27:08 +0530

    new package allPrograms diamond pattarn

--- New-Java_Again/src/allPrograms/StarPattarn.java
+++ New-Java_Again/src/allPrograms/StarPattarn.java
@@ -91,5 +91,31 @@ public class StarPattarn {
             System.out.println();
         }
         System.out.println();
+
+        for (int i=1;i<=count;i++){
+            for (int j=count-1;j>=i;j--){
+                System.out.print("  ");
+            }
+            for (int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            for (int l=2;l<=i;l++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i=2;i<=count;i++){
+            for (int j=2;j<=i;j++){
+                System.out.print("  ");
+            }
+            for (int k=count;k>=i;k--){
+                System.out.print("* ");
+            }
+            for (int l=count-1;l>=i;l--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
