c570a5db12a15df13cf1245947f421cdfb325605
Govind9066 <gadekargovind575@gmail.com>
2025-07-22 23:23:49 +0530

    new package allPrograms simple right angle triangle pattarn

---
+++ New-Java_Again/src/allPrograms/StarPattarn.java
@@ -0,0 +1,15 @@
+package allPrograms;
+
+public class StarPattarn {
+    public static void main(String[] args) {
+        int count=5;
+
+        for (int i=1;i<=count;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
