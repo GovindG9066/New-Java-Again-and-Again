187f15d6e8439e4e6f549cdab7d0685da4f4b05b
Govind9066 <gadekargovind575@gmail.com>
2025-07-23 15:39:43 +0530

    new package allPrograms empty square pattarn

--- New-Java_Again/src/allPrograms/StarPattarn.java
+++ New-Java_Again/src/allPrograms/StarPattarn.java
@@ -175,5 +175,18 @@ public class StarPattarn {
             System.out.println();
         }
         System.out.println();
+
+        for (int i=1;i<=count;i++){
+            for (int j=1;j<=count;j++){
+                if (i>1 && j>1 && i<count && j<count){
+                    System.out.print("  ");
+                }else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+
+
     }
 }
