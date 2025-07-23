8de5efd158f19cd1d8a2d56f44f802f25a5b1f22
Govind9066 <gadekargovind575@gmail.com>
2025-07-23 16:20:19 +0530

    new package allPrograms Number Pattarn  reverse like:
    5 4 3 2 1
    5 4 3 2
    5 4 3
    5 4
    5

--- New-Java_Again/src/allPrograms/StarPattarn.java
+++ New-Java_Again/src/allPrograms/StarPattarn.java
@@ -213,5 +213,12 @@ public class StarPattarn {
             System.out.println();
         }
         System.out.println();
+
+        for (int i=1;i<=count;i++){
+            for (int j=count;j>=i;j--){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
     }
 }
