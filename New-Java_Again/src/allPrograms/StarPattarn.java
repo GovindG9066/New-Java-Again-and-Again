544f0d5808f5a5e1bf2f77af0d1fa0a1f8aa6d3e
Govind9066 <gadekargovind575@gmail.com>
2025-07-23 15:07:00 +0530

    new package allPrograms X Pattarn Printed

--- New-Java_Again/src/allPrograms/StarPattarn.java
+++ New-Java_Again/src/allPrograms/StarPattarn.java
@@ -133,5 +133,47 @@ public class StarPattarn {
             System.out.println();
         }
         System.out.println();
+
+        for (int i=1;i<=count;i++){
+            for (int j=2;j<=i;j++){
+                System.out.print("  ");
+            }
+            for (int k=count;k>=i;k--){
+                if (k==count){
+                    System.out.print("* ");
+                }else {
+                    System.out.print("  ");
+                }
+            }
+            for (int l=count-1;l>=i;l--){
+                if (l>i){
+                    System.out.print("  ");
+                }else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i=2;i<=count;i++){
+            for (int j=count-1;j>=i;j--){
+                System.out.print("  ");
+            }
+            for (int k=1;k<=i;k++){
+                if (k!=1){
+                    System.out.print("  ");
+                }else {
+                    System.out.print("* ");
+                }
+            }
+            for (int l=2;l<=i;l++){
+                if (l<i){
+                    System.out.print("  ");
+                }else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
