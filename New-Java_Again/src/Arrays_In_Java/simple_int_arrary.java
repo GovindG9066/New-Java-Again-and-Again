ca868cd804654ab5abf78c91946d74e96b6c73e1
Govind9066 <gadekargovind575@gmail.com>
2025-07-24 16:51:44 +0530

    declearation+ creation+initialization+retrive in one line   Smart Programming

--- New-Java_Again/src/Arrays_In_Java/simple_int_arrary.java
+++ New-Java_Again/src/Arrays_In_Java/simple_int_arrary.java
@@ -12,5 +12,13 @@ public class simple_int_arrary {
         for (int i=0;i<no.length;i++){
             System.out.println(no[i]);
         }
+
+        System.out.println();
+
+        int[] a={10,20,30,40,50};
+
+        for (int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
     }
 }
