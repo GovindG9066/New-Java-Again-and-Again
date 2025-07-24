fbcc3565980d1d235aac4d9b80bb4fc92371a5b3
Govind9066 <gadekargovind575@gmail.com>
2025-07-24 16:49:26 +0530

    first simple array program from tha lector Smart Programming

---
+++ New-Java_Again/src/Arrays_In_Java/simple_int_arrary.java
@@ -0,0 +1,16 @@
+package Arrays_In_Java;
+
+public class simple_int_arrary {
+    public static void main(String[] args) {
+        int empNo[]=new int[5];
+
+        int[] no=new int[3];
+        no[0]=10;
+        no[1]=20;
+        no[2]=30;
+
+        for (int i=0;i<no.length;i++){
+            System.out.println(no[i]);
+        }
+    }
+}
