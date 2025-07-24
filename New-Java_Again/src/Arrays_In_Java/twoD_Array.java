fcc48d6575fc94dc2ab87f4de7c8bd1e0ab08b45
Govind9066 <gadekargovind575@gmail.com>
2025-07-24 20:34:03 +0530

    simple multidimensional array program

---
+++ New-Java_Again/src/Arrays_In_Java/twoD_Array.java
@@ -0,0 +1,20 @@
+package Arrays_In_Java;
+
+public class twoD_Array {
+    public static void main(String[] args) {
+        int[][] a= new int[2][3];
+        a[0][0]=10;
+        a[0][1]=20;
+        a[0][2]=30;
+        a[1][0]=40;
+        a[1][1]=50;
+        a[1][2]=60;
+
+
+
+        int[][] b={{10,20,30},{40,50,60}};
+        System.out.println(b[0][0]);
+
+
+    }
+}
