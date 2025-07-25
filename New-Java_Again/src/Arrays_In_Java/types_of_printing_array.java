1101fe429527eb64e87ba9cec04c2281b9b9c7e1
Govind9066 <gadekargovind575@gmail.com>
2025-07-25 14:25:43 +0530

    print array using the for loop

---
+++ New-Java_Again/src/Arrays_In_Java/types_of_printing_array.java
@@ -0,0 +1,16 @@
+package Arrays_In_Java;
+
+public class types_of_printing_array {
+    public static void main(String[] args) {
+
+        int[] a={10,20,30,40,50};
+
+        System.out.println("First : For Loop - ");
+
+        for (int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+
+    }
+}
