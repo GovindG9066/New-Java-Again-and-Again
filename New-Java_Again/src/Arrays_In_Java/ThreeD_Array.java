d73494f8ede99870e328ac7d024cefc22386946e
Govind9066 <gadekargovind575@gmail.com>
2025-07-25 13:36:01 +0530

    print overall multidimentional array printing any element in the array

---
+++ New-Java_Again/src/Arrays_In_Java/ThreeD_Array.java
@@ -0,0 +1,11 @@
+package Arrays_In_Java;
+
+public class ThreeD_Array {
+    public static void main(String[] args) {
+        int[][][] a={{{10,20,30},{40,50,60,70},{80,90}}};
+
+
+        System.out.println(a[0][2][1]);
+        }
+    }
+
