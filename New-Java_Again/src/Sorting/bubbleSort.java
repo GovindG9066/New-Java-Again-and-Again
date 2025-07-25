aed1ae6e5889bad13971434bea154376e98d1a97
Govind9066 <gadekargovind575@gmail.com>
2025-07-25 16:03:24 +0530

    Start sorting algorithem first program of bubble sort

---
+++ New-Java_Again/src/Sorting/bubbleSort.java
@@ -0,0 +1,29 @@
+package Sorting;
+
+public class bubbleSort {
+    public static void main(String[] args) {
+        int[] a={29,12,5,36,19};
+        int flag=0;
+        int temp=0;
+        for (int i=0;i<a.length;i++){
+            for (int j=0;j<a.length-1-i;j++){
+                if (a[j]>a[j+1]){
+                   temp=a[j];
+                   a[j]=a[j+1];
+                   a[j+1]=temp;
+
+                   flag=1;
+                }
+            }
+            if (flag==0){
+                break;
+            }
+
+        }
+        System.out.println("Bubble Sort : ");
+        for (int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+    }
+}
