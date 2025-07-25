0567da54cb5636eb3d6fa01da90f6150f3f251c6
Govind9066 <gadekargovind575@gmail.com>
2025-07-25 13:43:20 +0530

    print overall multidimentional array print all the array

--- New-Java_Again/src/Arrays_In_Java/ThreeD_Array.java
+++ New-Java_Again/src/Arrays_In_Java/ThreeD_Array.java
@@ -7,6 +7,15 @@ public class ThreeD_Array {
 
         System.out.println(a[0][2][1]);
         System.out.println(a[0].length);
+
+        for (int i=0;i<a.length;i++){
+            for (int j=0;j<a[i].length;j++){
+                for (int k=0;k<a[i][j].length;k++){
+                    System.out.print(a[i][j][k]+" ");
+                }
+            System.out.println();
+            }
+        }
         }
     }
 
