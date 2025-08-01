d264a0d45d7954d45d8977898f2d01069cd44691
Govind9066 <gadekargovind575@gmail.com>
2025-08-01 23:33:44 +0530

    changes to make the code logically correct

--- New-Java_Again/src/Sorting/bubbleSort.java
+++ New-Java_Again/src/Sorting/bubbleSort.java
@@ -3,9 +3,9 @@ package Sorting;
 public class bubbleSort {
     public static void main(String[] args) {
         int[] a={29,12,5,36,19};
-        int flag=0;
         int temp=0;
         for (int i=0;i<a.length;i++){
+            int flag=0;
             for (int j=0;j<a.length-1-i;j++){
                 if (a[j]>a[j+1]){
                    temp=a[j];
