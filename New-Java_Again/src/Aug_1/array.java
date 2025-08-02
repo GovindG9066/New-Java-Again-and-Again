3f1839cd04e81a7182349e4132b8f10dbcfa80be
Govind9066 <gadekargovind575@gmail.com>
2025-08-02 21:23:43 +0530

    bubble sort program

--- New-Java_Again/src/Aug_1/array.java
+++ New-Java_Again/src/Aug_1/array.java
@@ -46,12 +46,26 @@ public class array {
 
         System.out.println("Bubble Sort : ");
 
+        int temp=0;
         for (int i=0;i<arr.length;i++){
-            if (arr[i]>arr[i+1]){
-                arr[i]=arr[i+1];
+            int flag=0;
+            for (int j=0;j<arr.length-1-i;j++){
+                 if (arr[j]>arr[j+1]){
+                     temp=arr[j];
+                     arr[j]=arr[j+1];
+                     arr[j+1]=temp;
+
+                     flag=1;
+                 }
             }
-            System.out.println(arr[i]);
+                 if (flag==0){
+                     break;
+                 }
         }
+            for (int k:arr){
+                System.out.println(k);
+            }
+
 
     }
 }
