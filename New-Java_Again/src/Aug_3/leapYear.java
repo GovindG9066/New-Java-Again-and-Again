bf653e9354204c7e6b78d584a7b779b7cbb63379
Govind9066 <gadekargovind575@gmail.com>
2025-08-03 15:33:34 +0530

    Leap year

---
+++ New-Java_Again/src/Aug_3/leapYear.java
@@ -0,0 +1,13 @@
+package Aug_3;
+
+public class leapYear {
+    public static void main(String[] args) {
+        int year=2016;
+
+        if (year%400==0 || year%4==0 && year %100 !=0){
+            System.out.println("Leap Year ");
+        }else {
+            System.out.println("Not leap year;");
+        }
+    }
+}
