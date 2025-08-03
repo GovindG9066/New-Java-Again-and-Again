7b954ccfa6ed0983dd649f385fa6f028848586b1
Govind9066 <gadekargovind575@gmail.com>
2025-08-03 15:29:36 +0530

    factor of the number

---
+++ New-Java_Again/src/Aug_3/factorofNum.java
@@ -0,0 +1,13 @@
+package Aug_3;
+
+public class factorofNum {
+    public static void main(String[] args) {
+        int number=10;
+
+        for (int i=1;i<=number;i++){
+            if (number%i==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
