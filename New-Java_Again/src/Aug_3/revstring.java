3faa5fae950816fd83d412adca71f968fe08351c
Govind9066 <gadekargovind575@gmail.com>
2025-08-03 15:17:56 +0530

    reverse string

---
+++ New-Java_Again/src/Aug_3/revstring.java
@@ -0,0 +1,11 @@
+package Aug_3;
+
+public class revstring {
+    public static void main(String[] args) {
+         String name="Govind";
+
+         for (int i=name.length()-1;i>=0;i--){
+             System.out.print(name.charAt(i));
+         }
+    }
+}
