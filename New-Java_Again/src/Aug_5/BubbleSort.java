30fe2ae354c1f8e6b4c96721794b65c712e2198a
Govind9066 <gadekargovind575@gmail.com>
2025-08-05 19:59:50 +0530

    bubble Sort

---
+++ New-Java_Again/src/Aug_5/BubbleSort.java
@@ -0,0 +1,28 @@
+package Aug_5;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr={10,45,6,23,7,9,5};
+
+        int temp=0;
+
+        for (int i=0;i<=arr.length;i++){
+            int flag=0;
+            for (int j=0;j<arr.length-1;j++){
+                if (arr[j]>arr[j+1]){
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+
+                    flag=1;
+                }
+            }
+            if (flag==0){
+                break;
+            }
+        }
+        for (int k=0;k<=arr.length;k++){
+            System.out.println(arr[k]);
+        }
+    }
+}
