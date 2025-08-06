fcee567f324cb28a9e63a9e135a3b3386ad85c86
Govind9066 <gadekargovind575@gmail.com>
2025-08-06 23:07:27 +0530

    Selection Sort Program Complete

--- New-Java_Again/src/Aug_5/selectionSort.java
+++ New-Java_Again/src/Aug_5/selectionSort.java
@@ -9,7 +9,7 @@ public class selectionSort {
         for (int i=0;i<arr.length;i++){
             min=i;
             for (int j=i+1;j<arr.length;j++){
-                if (arr[j]<arr[i]){
+                if (arr[j]<arr[min]){
                     min=j;
                 }
             }
