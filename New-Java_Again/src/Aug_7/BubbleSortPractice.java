05f5831d2de3a05d34b2af63e5020cebb67eecc3
Govind9066 <gadekargovind575@gmail.com>
2025-08-07 20:28:47 +0530

    Bubble Sort Practice with int and String

--- New-Java_Again/src/Aug_7/BubbleSortPractice.java
+++ New-Java_Again/src/Aug_7/BubbleSortPractice.java
@@ -23,5 +23,27 @@ public class BubbleSortPractice {
             for (int k=0;k<arr.length;k++){
                 System.out.println(arr[k]);
             }
+        
+        String[] arr1={"Jethalal","Babitaji","Natukaka","Tarkamehta","Tapu","Champaklal"};
+        String  temp1="";
+        
+        for (int i=0;i<arr1.length;i++){
+            int flag=0;
+            for (int j=0;j<arr1.length-1-i;j++){
+                if (arr1[j].compareTo(arr1[j+1])>0){
+                    temp1=arr1[j];
+                    arr1[j]=arr1[j+1];
+                    arr1[j+1]=temp1;
+
+                    flag=1;
+                }
+            }
+            if (flag==0){
+                break;
+            }
+        }
+        for (int k=0;k<arr1.length;k++){
+            System.out.println(arr1[k]);
+        }
     }
 }
