7fc9192eec8c64f12e0ead6d73f2c40d84e54a44
Govind9066 <gadekargovind575@gmail.com>
2025-08-07 20:47:38 +0530

    Selection Sort Practice with int and String

--- New-Java_Again/src/Aug_7/SelectionSortPractice.java
+++ New-Java_Again/src/Aug_7/SelectionSortPractice.java
@@ -23,6 +23,25 @@ public class SelectionSortPractice {
         }
 
 
+        String[] arr1={"Nobita","Shijuka","Doremon","Dekisuki","Jiyan","Sunio"};
+        String temp1="";
+        int min1;
+
+        for (int i=0;i<arr1.length;i++){
+            min1=i;
+            for (int j=i+1;j<arr1.length;j++){
+                if (arr1[min1].compareTo(arr1[j])>0){
+                    min1=j;
+                }
+            }
+            temp1=arr1[i];
+            arr1[i]=arr1[min1];
+            arr1[min1]=temp1;
+        }
+
+        for (int k=0;k<arr1.length;k++){
+            System.out.println(arr1[k]);
+        }
 
     }
 }
