d87c94c81e77f4e492fc5e7b1e9359745dd38ccc
Govind9066 <gadekargovind575@gmail.com>
2025-08-09 19:23:30 +0530

    Selection Sort

--- New-Java_Again/src/Aug_9/selectionSort.java
+++ New-Java_Again/src/Aug_9/selectionSort.java
@@ -1,27 +1,23 @@
-package Aug_8;
+package Aug_9;
 
-public class SelectionSort {
+public class selectionSort {
     public static void main(String[] args) {
-        int[] arr={7,88,6,55,2,44,6,1,22};
-
-        int temp=0;
-        int min;
+        int[] arr={7,3,11,4,12,2};
+        int temp,min;
 
         for (int i=0;i<arr.length;i++){
             min=i;
             for (int j=i+1;j<arr.length;j++){
                 if (arr[min]>arr[j]){
                     min=j;
-
                 }
-            }
                 temp=arr[i];
                 arr[i]=arr[min];
                 arr[min]=temp;
-        }
-            for (int k=0;k<arr.length;k++){
-                System.out.println(arr[k]);
             }
-
+        }
+        for (int k=0;k<arr.length;k++){
+            System.out.println(arr[k]);
+        }
     }
 }
