d5eead9effd308ac0fa2abd4230aba0467349d8b
Govind9066 <gadekargovind575@gmail.com>
2025-08-13 23:39:11 +0530

    flip Star Pattern

--- New-Java_Again/src/Aug_13/AllStars.java
+++ New-Java_Again/src/Aug_13/AllStars.java
@@ -35,5 +35,16 @@ public class AllStars {
         System.out.println();
 
 
+        for (int i=1;i<=count;i++){
+            for (int j=count-1;j>=i;j--){
+                System.out.print("  ");
+            }
+            for (int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
     }
 }
