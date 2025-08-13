cd7d262301cbc07046be0124573c2aebb8aa7859
Govind9066 <gadekargovind575@gmail.com>
2025-08-13 23:37:32 +0530

    flip Star Pattern Up Side Down

--- New-Java_Again/src/Aug_13/AllStars.java
+++ New-Java_Again/src/Aug_13/AllStars.java
@@ -19,5 +19,21 @@ public class AllStars {
             }
             System.out.println();
         }
+
+        System.out.println();
+
+        for (int i=1;i<=count;i++){
+            for (int j=2;j<=i;j++){
+                System.out.print("  ");
+            }
+            for (int k=count;k>=i;k--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+
     }
 }
