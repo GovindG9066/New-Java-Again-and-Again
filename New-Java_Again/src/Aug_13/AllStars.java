9c497a94e4a4f3d2eadd5ef843e96a5c2470bcbf
Govind9066 <gadekargovind575@gmail.com>
2025-08-13 23:29:10 +0530

    Simple Star Pattern

---
+++ New-Java_Again/src/Aug_13/AllStars.java
@@ -0,0 +1,14 @@
+package Aug_13;
+
+public class AllStars {
+    public static void main(String[] args) {
+        int count=5;
+
+        for (int i=1;i<=count;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
