f9970de3a7f988c8bc760a993acf9dbc04b0ffe9
Govind9066 <gadekargovind575@gmail.com>
2025-08-13 23:35:53 +0530

    Simple Star Pattern Up Side Down

--- New-Java_Again/src/Aug_13/AllStars.java
+++ New-Java_Again/src/Aug_13/AllStars.java
@@ -10,5 +10,14 @@ public class AllStars {
             }
             System.out.println();
         }
+
+        System.out.println();
+
+        for (int i=1;i<=count;i++){
+            for (int j=count;j>=i;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
