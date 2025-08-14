e6ecd3e22f18ccae10439434b2a319d5460c69f9
Govind9066 <gadekargovind575@gmail.com>
2025-08-14 22:28:46 +0530

    Prime Number

---
+++ New-Java_Again/src/Aug_14/PrimeNum.java
@@ -0,0 +1,18 @@
+package Aug_14;
+
+public class PrimeNum {
+    public static void main(String[] args) {
+        int num=21;
+        int flag=0;
+        for (int i=2;i<num;i++){
+            if (num%i==0){
+                flag+=1;
+            }
+        }
+        if (flag==0){
+            System.out.println(" Prime");
+        }else {
+            System.out.println(" Not Prime");
+        }
+    }
+}
