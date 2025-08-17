0865181a35b22297a99181462dab4a96ebf7df72
Govind9066 <gadekargovind575@gmail.com>
2025-08-17 21:03:31 +0530

    swipe number

---
+++ New-Java_Again/src/Aug_17/swipeNumber.java
@@ -0,0 +1,14 @@
+package Aug_17;
+
+public class swipeNumber {
+    public static void main(String[] args) {
+        int num1=535,num2=865;
+
+        num1 = num1+num2;
+        num2=num1-num2;
+        num1=num1-num2;
+
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
