f64464d417b94b7faf9595d662267ccf709ad354
Govind9066 <gadekargovind575@gmail.com>
2025-08-19 21:03:19 +0530

    Pyramid Pattern with diagonal

--- New-Java_Again/src/Aug_19/simpleStar.java
+++ New-Java_Again/src/Aug_19/simpleStar.java
@@ -65,6 +65,19 @@ public class simpleStar {
         }
         System.out.println("--------------------------------------------");
 
+        for (int i=0;i<count;i++){
+            for (int j=count-2;j>=i;j--){
+                System.out.print("  ");
+            }
+            for (int k=0;k<=i;k++){
+                System.out.print("* ");
+            }
+            for (int l=1;l<=i;l++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------------------------------");
 
     }
 }
