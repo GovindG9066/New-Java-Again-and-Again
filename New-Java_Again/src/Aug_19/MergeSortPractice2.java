b9bf8d79f086852d7bf246db8ad61a4139357c5e
Govind9066 <gadekargovind575@gmail.com>
2025-08-19 20:47:51 +0530

    MergeSort Program practice 2

---
+++ New-Java_Again/src/Aug_19/MergeSortPractice2.java
@@ -0,0 +1,53 @@
+package Aug_19;
+
+public class MergeSortPractice2 {
+    public static void conqure(int arr[],int si,int mid,int ei){
+        int[] merged=new int[ei-si+1];
+
+        int idx1=si;
+        int idx2=mid+1;
+        int x=0;
+
+        while (idx1<=mid && idx2<=ei){
+            if (arr[idx1]<arr[idx2]){
+                merged[x++]=arr[idx1++];
+            }else {
+                merged[x++]=arr[idx2++];
+            }
+        }
+        while (idx1<=mid){
+            merged[x++]=arr[idx1++];
+
+        }
+        while (idx2<=ei){
+            merged[x++]=arr[idx2++];
+
+        }
+        for (int i=0,j=si;i< merged.length;i++,j++){
+            arr[j]=merged[i];
+        }
+    }
+    public static void divide(int arr[],int si,int ei){
+        if (si>=ei){
+            return;
+        }
+
+        int mid=si+(ei-si)/2;
+        divide(arr,si,mid);
+        divide(arr,mid+1,ei);
+
+        conqure(arr,si,mid,ei);
+    }
+    public static void main(String[] args) {
+        int[] arr={4,58,51,64,21,984,64,461,221};
+        int len= arr.length;
+
+        divide(arr,0,len-1);
+
+        for (int k:arr){
+            System.out.println(k);
+        }
+
+
+    }
+}
