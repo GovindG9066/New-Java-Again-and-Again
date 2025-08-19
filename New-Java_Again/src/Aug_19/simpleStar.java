91ce5fa7bd1493796c0dc9ed2821ed44b45d314b
Govind9066 <gadekargovind575@gmail.com>
2025-08-19 21:17:13 +0530

    Combination of the UP SIDE DOWN Pyramid triangle star pattern with diagonal and Pyramid triangle star pattern with diagonal

--- New-Java_Again/src/Aug_19/simpleStar.java
+++ New-Java_Again/src/Aug_19/simpleStar.java
@@ -128,6 +128,33 @@ public class simpleStar {
         System.out.println("--------------------------------------------");
 
 
+        for (int i=0;i<count;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("  ");
+            }
+            for (int k=count-1;k>=i;k--){
+                System.out.print("* ");
+            }
+            for (int l=count-2;l>=i;l--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i=1;i<count;i++){
+            for (int j=count-2;j>=i;j--){
+                System.out.print("  ");
+            }
+            for (int k=0;k<=i;k++){
+                System.out.print("* ");
+            }
+            for (int l=1;l<=i;l++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------------------------------");
+
+
 
     }
 }
