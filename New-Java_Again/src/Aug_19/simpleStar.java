42e2f0287c3843d7ce08bb07f7a479bed3aace2a
Govind9066 <gadekargovind575@gmail.com>
2025-08-19 21:00:32 +0530

    Up side down Pyramid Pattern without diagonal

--- New-Java_Again/src/Aug_19/simpleStar.java
+++ New-Java_Again/src/Aug_19/simpleStar.java
@@ -52,6 +52,19 @@ public class simpleStar {
             System.out.println();
 
         }
+        System.out.println("--------------------------------------------");
+
+        for (int i=0;i<count;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for (int k=count-1;k>=i;k--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------------------------------");
+
 
     }
 }
