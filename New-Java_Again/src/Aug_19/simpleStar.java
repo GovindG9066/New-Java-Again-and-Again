24e1d9eb0de11561ea5adc8db5c4f2a920a2293d
Govind9066 <gadekargovind575@gmail.com>
2025-08-19 21:08:18 +0530

    Combination of the simple and up side down right angle triangle star pattern

--- New-Java_Again/src/Aug_19/simpleStar.java
+++ New-Java_Again/src/Aug_19/simpleStar.java
@@ -93,6 +93,20 @@ public class simpleStar {
         }
         System.out.println("--------------------------------------------");
 
+        for (int i=0;i<count;i++){
+            for (int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i=1;i<count;i++){
+            for (int j=count-1;j>=i;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------------------------------");
+
 
     }
 }
