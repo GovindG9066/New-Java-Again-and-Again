43d47e4a39bd7114d84695b9c67474ffddd54ce0
Govind9066 <gadekargovind575@gmail.com>
2025-08-19 20:54:43 +0530

    flip Simple Star Pattern

--- New-Java_Again/src/Aug_19/simpleStar.java
+++ New-Java_Again/src/Aug_19/simpleStar.java
@@ -19,5 +19,18 @@ public class simpleStar {
             System.out.println();
         }
         System.out.println("------------------------------------------");
+
+        for (int i=0;i<count;i++){
+            for (int j=count-2;j>=i;j--){
+                System.out.print("  ");
+            }
+            for (int k=0;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------------------------------");
+
+
     }
 }
