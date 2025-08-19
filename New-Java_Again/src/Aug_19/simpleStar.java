eb759227fc6f49aa1802667ee602f5352e97ef8b
Govind9066 <gadekargovind575@gmail.com>
2025-08-19 20:52:04 +0530

    Simple Star Pattern Up side down

--- New-Java_Again/src/Aug_19/simpleStar.java
+++ New-Java_Again/src/Aug_19/simpleStar.java
@@ -10,5 +10,14 @@ public class simpleStar {
             }
             System.out.println();
         }
+        System.out.println("-----------------------------------------");
+
+        for (int i=0;i<count;i++){
+            for (int j=count-1;j>=i;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("------------------------------------------");
     }
 }
