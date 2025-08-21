fa28f868e2c9dbf66f846fefac5a988eba629cd5
Govind9066 <gadekargovind575@gmail.com>
2025-08-21 15:22:20 +0530

    Bubble Sort solve the incorrect statement

---
+++ New-Java_Again/src/Aug_21/selectionSort.java
@@ -0,0 +1,13 @@
+package Aug_21;
+
+public class selectionSort {
+    public static void main(String[] args) {
+        int[] arr={8,65,148,4,64,4,34,34};
+        int temp=0,min;
+
+        for (int i=0;i< arr.length;i++){
+            min=i;
+
+        }
+    }
+}
