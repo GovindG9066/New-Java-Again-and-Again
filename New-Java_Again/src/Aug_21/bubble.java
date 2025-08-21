0d5630b8bcb684166717f331602d00971211ac14
Govind9066 <gadekargovind575@gmail.com>
2025-08-21 15:28:37 +0530

    Selection sort

--- New-Java_Again/src/Aug_21/bubble.java
+++ New-Java_Again/src/Aug_21/bubble.java
@@ -7,11 +7,11 @@ public class bubble {
 
         for (int i=0;i< arr.length;i++){
             int flag=0;
-            for (int j=i+1;j< arr.length;j++){
-                if (arr[i]>arr[j]){
-                    temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+            for (int j=0;j< arr.length-1-i;j++){
+                if (arr[j]>arr[j+1]){
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
 
                     flag+=1;
 
--- New-Java_Again/src/Aug_21/selectionSort.java
+++ New-Java_Again/src/Aug_21/selectionSort.java
@@ -7,7 +7,18 @@ public class selectionSort {
 
         for (int i=0;i< arr.length;i++){
             min=i;
+            for (int j=i+1;j< arr.length;j++){
+                if (arr[min]>arr[j]){
+                    min=j;
+                }
+            }
+                temp=arr[i];
+                arr[i]=arr[min];
+                arr[min]=temp;
+        }
 
+        for (int k:arr){
+            System.out.println(k);
         }
     }
 }
