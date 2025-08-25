2407acc07c12e3f677bace3a24fe02e7b3f3296b
Govind9066 <gadekargovind575@gmail.com>
2025-08-25 22:35:06 +0530

    Right angle triangle star pattern flip

--- New-Java_Again/src/Aug_25/StarPrograms.java
+++ New-Java_Again/src/Aug_25/StarPrograms.java
@@ -20,5 +20,17 @@ public class StarPrograms {
         }
 
         System.out.println();
+
+        for (int i=1;i<=count;i++){
+            for (int j=2;j<=i;j++){
+                System.out.print("  ");
+            }
+            for (int k=count;k>=i;k--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
     }
 }
