4913d9ac596db9eaa1236e1db917fec04379d703
Govind9066 <gadekargovind575@gmail.com>
2025-08-25 22:33:42 +0530

    Right angle triangle star pattern

---
+++ New-Java_Again/src/Aug_25/StarPrograms.java
@@ -0,0 +1,14 @@
+package Aug_25;
+
+public class StarPrograms {
+    public static void main(String[] args) {
+        int count=7;
+
+        for (int i=1;i<=count;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
