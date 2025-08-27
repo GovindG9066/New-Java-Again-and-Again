28b4a042a96bd1f217da20cfda30254bebd81ea3
Govind9066 <gadekargovind575@gmail.com>
2025-08-27 21:09:15 +0530

    Simple Right Angle Triangle Star Patter

--- New-Java_Again/src/Aug_27/insertionSort.java
+++ New-Java_Again/src/Aug_27/insertionSort.java
@@ -3,6 +3,8 @@ package Aug_27;
 public class insertionSort {
     public static void main(String[] args) {
         int[] arr={485,8,51,6,51,51,51,5};
-        int temp=0;
+        int temp=0,original;
+
+
     }
 }
