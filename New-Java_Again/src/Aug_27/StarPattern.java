aadc3b3292469092a6e8e7dc6fb9a8d8e1d8d0bc
Govind9066 <gadekargovind575@gmail.com>
2025-08-27 21:10:45 +0530

    Simple Up Side Down Right Angle Triangle Star Patter

---
+++ New-Java_Again/src/Aug_27/StarPattern.java
@@ -0,0 +1,23 @@
+package Aug_27;
+
+public class StarPattern {
+    public static void main(String[] args) {
+        int count=5;
+
+        for (int i=0;i<count;i++){
+            for (int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i=0;i<=count;i++){
+            for (int j=count;j>i;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
