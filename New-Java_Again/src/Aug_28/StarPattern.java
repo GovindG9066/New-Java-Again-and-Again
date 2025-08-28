24edec17c97104cbcaf866652b62c639e3d05d4e
Govind9066 <gadekargovind575@gmail.com>
2025-08-28 21:25:17 +0530

    combination of the pyramid triangle pattern simple up side down diamond pattern

--- New-Java_Again/src/Aug_28/StarPattern.java
+++ New-Java_Again/src/Aug_28/StarPattern.java
@@ -30,5 +30,32 @@ public class StarPattern {
             System.out.println();
         }
         System.out.println();
+
+        for (int i=0;i<=count;i++){
+            for (int j=count;j>i;j--){
+                System.out.print("  ");
+            }
+            for (int k=0;k<i;k++){
+                System.out.print("* ");
+            }
+            for (int l=1;l<i;l++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i=1;i<count;i++){
+            for (int j=0;j<i;j++){
+                System.out.print("  ");
+            }
+            for (int k=count;k>i;k--){
+                System.out.print("* ");
+            }
+            for (int l=count-1;l>i;l--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
