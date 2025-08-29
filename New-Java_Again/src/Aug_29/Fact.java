e4ccf926eb7099fbae6cca2e2dbda33dafb7ab6e
Govind9066 <gadekargovind575@gmail.com>
2025-08-29 22:47:33 +0530

    Factor of the Number

---
+++ New-Java_Again/src/Aug_29/Fact.java
@@ -0,0 +1,14 @@
+package Aug_29;
+
+
+public class Fact {
+    public static void main(String[] args) {
+        int num=15;
+
+        for (int i=1;i<=num;i++){
+            if (num%i==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
