0a1af2e903bf3b58666c11dd66d4db814437a824
Govind9066 <gadekargovind575@gmail.com>
2025-08-31 22:39:00 +0530

    Flip up side down right angle triangle

--- New-Java_Again/src/Aug_31/All_Star_Patterns.java
+++ New-Java_Again/src/Aug_31/All_Star_Patterns.java
@@ -18,5 +18,16 @@ public class All_Star_Patterns {
             }
             System.out.println();
         }
+        System.out.println();
+
+        for (int i=0;i<=count;i++){
+            for (int j=0;j<i;j++){
+                System.out.print("  ");
+            }
+            for (int k=count;k>i;k--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
